937ee7a993eb3061dd7d0fd80fb4b2d762b0c110
Diego Dorado <diegodorado@gmail.com>
2025-06-14 12:53:31 -0300

    add banana split

---
+++ keyboards/banana_split/config.h
@@ -0,0 +1,12 @@
+#pragma once
+
+// Matrix size
+#define MATRIX_ROWS (4 * 2)
+#define MATRIX_COLS 6
+
+#define MASTER_LEFT
+
+#define BOOTMAGIC_ROW 0
+#define BOOTMAGIC_COLUMN 0
+
+#define SELECT_SOFT_SERIAL_SPEED 5
