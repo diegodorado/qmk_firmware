afbcba4d468aaca0c8bec38d68b57a58b3fd57ee
Diego Dorado <diegodorado@gmail.com>
2025-06-14 13:11:43 -0300

    switch esc/tab

--- keyboards/banana_split/keymaps/default/keymap.c
+++ keyboards/banana_split/keymaps/default/keymap.c
@@ -35,8 +35,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      *                             +-------------+                             +-------------+
      */
     [_BASE] = LAYOUT(
-        KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,               KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS,
-        KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,               KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,               KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS,
+        KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,               KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,               KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_BSLS,
                                   KC_SPC, KC_BSPC, KC_GRV,             KC_LGUI, KC_LALT, KC_ENT,
                                             RAISE,   KC_SPC,             KC_ENT,  LOWER
@@ -48,7 +48,7 @@ void keyboard_post_init_user(void) {
   // Customise these values to desired behaviour
   debug_enable=true;
   debug_matrix=true;
-  //debug_keyboard=true;
+  debug_keyboard=true;
   //debug_mouse=true;
 }
 
@@ -56,18 +56,4 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   // If console is enabled, it will print the matrix position and status of each key pressed
   uprintf("KL: kc: 0x%04X, col: %2u, row: %2u, pressed: %u, time: %5u, int: %u, count: %u\n", keycode, record->event.key.col, record->event.key.row, record->event.pressed, record->event.time, record->tap.interrupted, record->tap.count);
   return true;
-}
-
-void matrix_scan_user(void) {
-    static bool once = false;
-    if (!once) {
-        dprintf("Matrix scan started\n");
-        once = true;
-
-        if (is_keyboard_master()) {
-            dprintf("I'm the master\n");
-        } else {
-            dprintf("I'm the slave\n");
-        }
-    }
 }
